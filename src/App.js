158410c50d43d55d210d63dd6eb3006af9da770c
VitaliPobudei <vitali.pobudei@gmail.com>
2021-11-29 03:24:24 +0300

    User Input and

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import AddUser from "./components/Users/AddUser";
+import UserList from "./components/Users/UserList";
 
 
 function App() {
+    const [userList, setUserList] = useState([]);
+
+    const addUserHandler = (uName, uAge) => {
+        setUserList((prevUseList) => {
+            return [
+                ...prevUseList,
+                { name: uName, age: uAge, id: Math.random().toString() },
+            ];
+        });
+    };
+
   return (
     <div>
-      <AddUser />
+      <AddUser onAddUser={addUserHandler} />
+      <UserList users={userList} />
     </div>
   );
 }
